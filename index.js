b73872ec1e9c877a295505b369030bb5106e8a0b
Przemysław Zalewski <ciunkos@gmail.com>
2022-10-11 19:39:00 +0200

    Prepare simple server setup

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import { readFile } from "fs/promises";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let indexFilePromise = undefined;
+app.get("/", async (_, res, next) => {
+  try {
+    const contents = await (indexFilePromise ??
+      (indexFilePromise = readFile("./index.html", "utf-8")));
+
+    res.contentType("text/html;charset=UTF-8").send(contents);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("/echo", (req, res) => {
+  res.send({
+    body: req.body,
+    cookies: req.cookies,
+    headers: req.headers,
+    httpVersion: req.httpVersion,
+    ip: req.ip,
+    ips: req.ips,
+    method: req.method,
+    params: req.params,
+    path: req.path,
+    query: req.query,
+    url: req.url,
+    xyz: req.hostname,
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
